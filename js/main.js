40a9882661e0893641d6a30e95dada081a9f9e10
Emma Beswick <emmabwp@hotmail.com>
2017-11-25 17:06:20 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var current = 'homePage';
+var isOpen = 0;
+
+window.onload = function () {
+    var a = $(window).height();
+    var b = (document.getElementById('mytopBar').offsetHeight);
+    var c = a - b;
+    var c = c + "px";
+    document.getElementById("myContentArea").style.height = c;
+};
+
+
+function openNav() {
+   console.log("openNav()");
+   isOpen=1;
+   document.getElementById("myTopnav").style.width = "250px";
+}
+function navSwitch() {
+    console.log(isOpen);
+    if (isOpen==0){      
+        openNav();
+    }else{        
+        closeNav();
+    }
+}
+
+
+function closeNav() {
+    console.log("closeNav()");
+    isOpen = 0;
+    document.getElementById("myTopnav").style.width = "0px";
+}
+
+function show(shown) {
+    isOpen = 0;
+    console.log("show()" + shown);
+    
+    document.getElementById("myTopnav").style.width = "0";
+
+    document.getElementById(shown).style.display = 'block';
+
+    if (current) {
+        document.getElementById(current).style.display = 'none';
+    }
+    current = shown;
+
+}
